e7ca581ccc849a2d9e07278f86796ef53565d518
Antony Ronaldo A <antonyronaldoa2006@gmail.com>
2026-05-17 17:09:51 +0530

    Added new features and improved code

--- vite-project/src/components/InputContainer.jsx
+++ vite-project/src/components/InputContainer.jsx
@@ -1,13 +1,23 @@
 function InputContainer({ inputValue, writeToDo, addToDo }) {
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addToDo();
+    }
+  };
+
   return (
     <div className="input-container">
       <input
         type="text"
         value={inputValue}
         onChange={writeToDo}
-        placeholder="Leave a Note"
+        onKeyPress={handleKeyPress}
+        placeholder="Add a new task... (Press Enter to add)"
+        className="todo-input"
       />
-      <button onClick={addToDo}>+</button>
+      <button onClick={addToDo} className="add-btn" title="Add task (Enter)">
+        +
+      </button>
     </div>
   );
 }
